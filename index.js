cea929cfdcb57458693c1fb8b5cb7b2adc0c55f2
Your_Name <joseph161216@gmail.com>
2020-07-12 01:25:40 -0700

    install knex and objection

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const cors = require("cors");
 
 //middleware
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//app.use(allow(allowedOrigins)); <- should i use this?
+//app.use(helmet()); <- should i use this?
 
 app.listen(5000, () => {
   console.log("Server has started on port 5000")
